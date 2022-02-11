ac92b7ef2c008a9d60f551c4d5dafb830286a10d
Ward Lootens <wardl@mips.be>
2022-02-11 21:17:14 +0100

    Add initial version of sleeptrainer script
    Usage:
    ./sleeptrainer.sh [COLOR [LOW_LIGHT] ]
        COLOR       Either 'black', 'grey', 'red', 'orange', 'yellow',
                    'green', 'blue' or 'white'.
                    Defaults to 'white'.
        LOW_LIGHT   Either 'True' or 'False'. Defaults to 'True'.

---
+++ sleeptrainer.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python
+
+import sys
+
+from sense_hat import SenseHat
+
+colors = {
+        'black'  : (0, 0, 0),
+        'grey'   : (128, 128, 128),
+        'red'    : (255, 0, 0),
+        'blue'   : (0, 0, 255),
+        'green'  : (0, 255, 0),
+        'yellow' : (255, 255, 0),
+        'orange' : (255, 165, 0),
+        'white'  : (255, 255, 255)
+        }
+
+color_name = sys.argv[1] if len(sys.argv) > 1 else 'black'
+low_light = (sys.argv[2] == 'True') if len(sys.argv) > 2 else True
+
+color = colors[color_name] if color_name in colors else colors['black']
+k = colors['black']
+
+print 'Setting color to: ', color_name, '(',  color, '), low_light = ', low_light
+
+sense = SenseHat()
+sense.low_light = low_light
+#sense.low_light = True
+#sense.low_light = False
+# sense.clear(g)
+# sense.clear(y)
+
+def draw_circle(x):
+    return [
+            k, k, k, k, k, k, k, k,
+            k, k, k, k, k, k, k, k,
+            k, k, k, x, x, k, k, k,
+            k, k, x, x, x, x, k, k,
+            k, k, x, x, x, x, k, k,
+            k, k, k, x, x, k, k, k,
+            k, k, k, k, k, k, k, k,
+            k, k, k, k, k, k, k, k
+        ]
+
+sense.set_pixels(draw_circle(color))
+
